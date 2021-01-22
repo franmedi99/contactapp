eaf0e8798112e54632d44bd5dcb121d0d92f3fb7
Franciscorp <franmedi_mdp@hotmail.com>
2021-01-22 12:14:45 -0300

    agregando mas variables de entorno

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,11 @@
 module.exports = {
 
     database: {
-        connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'db_links'
+        connectionLimit: 30,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
 
 };
